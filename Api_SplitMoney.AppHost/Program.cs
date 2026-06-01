5d9be0d99a4f7194c94de196abd929892780fa94
lucas ludueña <lucas.ludu.mdp@hotmail.com>
2026-06-01 13:48:46 -0300

    Guardando route de Front en el AppHost de Aspire

--- Api_SplitMoney.AppHost/Program.cs
+++ Api_SplitMoney.AppHost/Program.cs
@@ -1,5 +1,8 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.WebApi>("webapi");
+var webapi = builder.AddProject<Projects.WebApi>("webapi");
+
+builder.AddExecutable("frontend", "dotnet", "c:\\Users\\Usuario\\source\\repos\\lucasludu\\View_SplitMoney", "run", "--project", "SplitMoney.Client.csproj", "-f", "net10.0-windows10.0.19041.0")
+    .WithReference(webapi);
 
 builder.Build().Run();
